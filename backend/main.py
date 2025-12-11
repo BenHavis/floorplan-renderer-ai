acf33c7fc08723132131d603decd1a3dfef9bde3
BenHavis <ben.havis1@gmail.com>
2025-12-11 13:56:13 -0500

    update cors

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://floorplan-renderer-ai.vercel.app",
+    "http://localhost:3000"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
